f008e74f35133920a7835a7971284c58d09b9708
Dejan Andrejic <andrejicdejan93@gmail.com>
2025-06-15 18:39:50 +0200

    fix

--- NoteBack/models/note.js
+++ NoteBack/models/note.js
@@ -1,6 +1,7 @@
 // mangoDB
 const url = process.env.MONGODB_URI
 const mongoose = require('mongoose')
+mongoose.set('strictQuery',false)
 
 console.log('connecting to ', url)
 mongoose.connect(url)
@@ -11,18 +12,17 @@ mongoose.connect(url)
     console.log('error connecting to MongoDB:', err.message)
   })
 
-mongoose.set('strictQuery',false)
 const noteSchema = new mongoose.Schema({
   content: String,
   important: Boolean,
 })
 
 noteSchema.set('toJSON', {
-  transform: (document, returnedObject => {
+  transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
     delete returnedObject.__v
-  })
+  }
 })
 
 module.exports = mongoose.model('Note', noteSchema)
