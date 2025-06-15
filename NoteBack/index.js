4062daae22cdeec0f1d35d521d3c6c40f9000c26
Dejan Andrejic <andrejicdejan93@gmail.com>
2025-06-15 18:17:19 +0200

    mangoDB

--- NoteBack/index.js
+++ NoteBack/index.js
@@ -1,17 +1,11 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const Note = require('./models/note.js')
 app.use(express.static('dist'))
 app.use(express.json())
 
-let notes = [
-   {
-     id: "1",    content: "HTML is easy",    important: true  
-   }, {
-     id: "2",    content: "Browser can execute only JavaScript",    important: false  
-   },  {
-     id: "3",    content: "GET and POST are the most important methods of HTTP protocol",    important: true  
-   }
-]
+let notes = []
 
 // ROUTES
 // ------------------------------------
@@ -19,49 +13,46 @@ let notes = [
 app.get('/', (req,res) => {
   res.send('<h1>Hello World!</h1>')
 })
+
 // notes
 app.get('/api/notes', (req,res) => {
-  res.json(notes)
-})
-// get specific note
-app.get('/api/notes/:id', (req,res) => {
-const id = req.params.id
-const note = notes.find(note => note.id === id)
-  if (note) {
-    res.json(note)
-  } else {
-    res.status(404).end()
-  }
-})
-// delete note
-app.delete('/api/notes/:id', (req,res) => {
-  const id = req.params.id
-  notes = notes.filter(note => note.id !== id)
-  res.status(204).end()
-})
-// add notes 
-app.post('/api/notes', (req,res) => {
-  const body = req.body
-  if (!body.content) {
-    return res.status(400).json({
-      error: 'content missing'
-    }) 
-  }
+  Note.find({}).then(notes => {
+    res.json(notes)
+    })
+  })
 
-  const maxID = notes.length > 0
-    ? Math.max(...notes.map(n => Number(n.id))) : 0
-  note.id = String(maxID + 1)
+ // get specific note
+ app.get('/api/notes/:id', (req,res) => {
+   Note.findById(req.params.id).then(note => {
+     res.json(note)
+   })
+ })
 
-  const note = {
-    content: body.content,
-    important: body.important || false,
-    id: note.id
-  }
-  notes = notes.concat(note)
-  res.json(note)
+ // delete note
+ app.delete('/api/notes/:id', (req,res) => {
+   const id = req.params.id
+   notes = notes.filter(note => note.id !== id)
+   res.status(204).end()
+ })
+
+ // add notes 
+ app.post('/api/notes', (req,res) => {
+   const body = req.body
+   if (!body.content) {
+     return res.status(400).json({
+       error: 'content missing'
+     }) 
+   }
+   const note = new Note ({
+     content: body.content,
+     important: body.important || false,
+   })
+   note.save().then(saved => {
+     res.json(saved)
+   })
 })
-// 
-const PORT = process.env.PORT || 3001
+ 
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
